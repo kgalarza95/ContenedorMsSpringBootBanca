020dd306b098970cda260ba96b721d8c891d4d42
Kevin Galarza Vera <44540130+kgalarza95@users.noreply.github.com>
2024-09-23 00:56:55 -0500

    Inicio del proyecto

---
+++ src/main/java/com/test/banco/cliente/MsGestionClientesApplication.java
@@ -0,0 +1,13 @@
+package com.test.banco.cliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsGestionClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsGestionClientesApplication.class, args);
+	}
+
+}
